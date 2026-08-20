22f5d11701cc1f5188199e5a46532f1ae882ed54
Matija Almaši <almasimatija@gmail.com>
2026-08-20 20:01:35 +0200

    add cache settings

--- Application/DependancyInjection.cs
+++ Application/DependancyInjection.cs
@@ -1,15 +1,22 @@
-﻿using Application.Abstractions.Services;
+﻿thought
+<channel|>```csharp
+using Application.Abstractions.Services;
 using Application.Services;
+using Application.Options;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace Application;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApplication(this IServiceCollection services)
+    public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration)
     {
+        services.Configure<CacheSettings>(configuration.GetSection("CacheSettings"));
         services.AddTransient<IMovieTrailerService, MovieTrailerService>();
 
         return services;
     }
 }
+
---
+++ Application/Options/CacheSettings.cs
@@ -0,0 +1,7 @@
+namespace Application.Options;
+
+public class CacheSettings
+{
+    public int MovieListExpirationHours { get; set; }
+    public int MovieDetailExpirationHours { get; set; }
+}
